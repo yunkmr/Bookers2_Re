0967e118c5806f7358096b74263f663b5b6aae52
EC2 Default User <ec2-user@ip-172-31-95-24.ec2.internal>
2021-11-25 07:59:52 +0000

    [Update] いいね機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :books
+  has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
   attachment :profile_image, destroy: false
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users
-  resources :books
+  resources :books do
+    resource :favorites, only: [:create, :destroy]
+  end
 
   root 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
